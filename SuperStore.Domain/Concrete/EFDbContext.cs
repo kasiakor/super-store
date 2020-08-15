9fd3c1a135563e5665b6f78cc6b54bb359c55e11
kasia.kordys <kasia.kordys@gmail.com>
2020-08-15 19:52:14 +0100

    create a context class

---
+++ SuperStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,14 @@
+﻿using SuperStore.Domain.Entities;
+using System.Data.Entity;
+
+namespace SuperStore.Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        //DbContext class automatically defines a property for the table
+        //the name of the property specifies the table: Products
+        //the type parameter of the DbSet result specifies the model type: Product
+        // EF should use the Product model type to represent rows in the Products table
+        public DbSet<Product> Products { get; set; }
+    }
+}
