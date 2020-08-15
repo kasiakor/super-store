5389d3eed36441432823369558533df78a49cb5a
kasia.kordys <kasia.kordys@gmail.com>
2020-08-15 20:12:25 +0100

    add repository class

---
+++ SuperStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using SuperStore.Domain.Abstract;
+using SuperStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SuperStore.Domain.Concrete
+{
+    //repository class. It implements the IProductRepository interface and uses an instance of EFDbContext to retrieve data from the database using the EF
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
