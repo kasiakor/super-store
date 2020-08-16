173a8b257dd05d9ad0ed23ea964d78c380fec2ca
kasia.kordys <kasia.kordys@gmail.com>
2020-08-16 19:52:58 +0100

    add pagination logic to controller

--- SuperStore.WebUI/Controllers/ProductController.cs
+++ SuperStore.WebUI/Controllers/ProductController.cs
@@ -12,15 +12,20 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
+
         //constructor that declares a dependency on the IProductRepository interface, Ninject will inject the dependency for the product repository when it instantiates the controller class
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((page - 1) * PageSize)
+            .Take(PageSize));
         }
     }
 }
