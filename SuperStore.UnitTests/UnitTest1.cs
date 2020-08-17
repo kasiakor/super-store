c2041a7f0746efc1d1b89d9cb4dc656448882afa
kasia.kordys <kasia.kordys@gmail.com>
2020-08-17 21:48:18 +0100

    add can_generate_page_links unit test

--- SuperStore.UnitTests/UnitTest1.cs
+++ SuperStore.UnitTests/UnitTest1.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SuperStore.Domain.Abstract;
 using SuperStore.Domain.Entities;
 using SuperStore.WebUI.Controllers;
+using SuperStore.WebUI.HtmlHelpers;
+using SuperStore.WebUI.Models;
 
 namespace SuperStore.UnitTests
 {
@@ -26,7 +30,7 @@ namespace SuperStore.UnitTests
             new Product {ProductID = 3, Name = "P3"},
             new Product {ProductID = 4, Name = "P4"},
             new Product {ProductID = 5, Name = "P5"}
-});
+            });
             //mock repository injected to the constructor of the class
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
@@ -40,6 +44,33 @@ namespace SuperStore.UnitTests
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
             Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+
+            // Arrange - define an HTML helper - we need to do this
+            // in order to apply the extension method
+            HtmlHelper myHelper = null;
+
+            // Arrange - create PagingInfo data
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // Arrange - set up the delegate using a lambda expression
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            // Act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            // Assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"+@"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"+@"<a class=""btn btn-default"" href=""Page3"">3</a>",
+                result.ToString());
         }
     }
 }
--- SuperStore.WebUI/HtmlHelpers/PagingHelpers.cs
+++ SuperStore.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -15,15 +15,15 @@ namespace SuperStore.WebUI.HtmlHelpers
             StringBuilder result = new StringBuilder();
             for (int i = 1; i <= pagingInfo.TotalPages; i++)
             {
-                TagBuilder tag = new TagBuilder(" a ");
-                tag.MergeAttribute(" href ", pageUrl(i));
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
                 tag.InnerHtml = i.ToString();
                 if (i == pagingInfo.CurrentPage)
                 {
-                    tag.AddCssClass(" selected ");
-                    tag.AddCssClass(" btn - primary ");
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
                 }
-                tag.AddCssClass(" btn btn-default ");
+                tag.AddCssClass("btn btn-default");
                 result.Append(tag.ToString());
             }
             return MvcHtmlString.Create(result.ToString());
