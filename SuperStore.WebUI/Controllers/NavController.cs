5a8b2b4773be8ff069fe286362f2ad19e4d1de99
kasia.kordys <kasia.kordys@gmail.com>
2020-08-20 17:20:44 +0100

    add category nav menu

---
+++ SuperStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,27 @@
+﻿using SuperStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        // GET: Nav
+       
+            private IProductRepository repository;
+        
+            public NavController(IProductRepository repo)
+            {
+                repository = repo;
+            }
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products.Select(x => x.Category).Distinct().OrderBy(x => x);
+     
+            return PartialView(categories);
+        }
+    }
+}
