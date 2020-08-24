04369dffe7e624985e01ef2b472e2f0514637223
kasia.kordys <kasia.kordys@gmail.com>
2020-08-24 20:29:13 +0100

    add cart model binder

--- SuperStore.WebUI/Controllers/CartController.cs
+++ SuperStore.WebUI/Controllers/CartController.cs
@@ -1,8 +1,8 @@
-﻿using System.Linq;
-using System.Web.Mvc;
+﻿using SuperStore.Domain.Abstract;
 using SuperStore.Domain.Entities;
-using SuperStore.Domain.Abstract;
 using SuperStore.WebUI.Models;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace SuperStore.WebUI.Controllers
 {
@@ -16,52 +16,61 @@ namespace SuperStore.WebUI.Controllers
             repository = repo;
         }
 
-        public ViewResult Index(string returnUrl)
+        //public ViewResult Index(string returnUrl)
+        public ViewResult Index(Cart cart, string returnUrl)
         {
             return View(new CartIndexViewModel
             {
-                Cart = GetCart(),
+                //Cart = GetCart(),
+                Cart = cart,
                 ReturnUrl = returnUrl
             });
         }
         //parameters match input elements in the html form
-        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        //for model binding add Cart cart
+        //mvc receives request for addto cart method it looks at itys params and tries to find binders to create instances
+        //custom binder is asked to create cart object by working with the session state feature 
+        public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl)
         {
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null)
             {
-                GetCart().AddItem(product, 1);
+                cart.AddItem(product, 1);
+                //GetCart().AddItem(product, 1);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl)
         {
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null)
             {
-                GetCart().RemoveLine(product);
+                cart.RemoveLine(product);
+                //GetCart().RemoveLine(product);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        //we use session state feature - GetCart() - to store and retrieve cart objects. It uses cooking or Url rewriting to associate multiple requests to form a single browsing session/cart persistend between requests
-        private Cart GetCart()
-        {
-            // Session["Cart"] = cart, adds object to session state, set the value for a key on the Session object
-            //Cart cart = (Cart)Session["Cart"], read the value, retrieve the object
-            //session objects are stored in the memory of theASP.NET server by default
-            Cart cart = (Cart)Session["Cart"];
-            if (cart == null)
-            {
-                cart = new Cart();
-                Session["Cart"] = cart;
-            }
-            return cart;
-        }
+
+        ////removed for model binding!!!
+        ////we use session state feature - GetCart() - to store and retrieve cart objects. It uses cookies or Url rewriting to associate multiple requests to form a single browsing session/cart persistent between requests
+        //private Cart GetCart()
+        //{
+        //    // Session["Cart"] = cart, adds object to session state, set the value for a key on the Session object
+        //    //Cart cart = (Cart)Session["Cart"], read the value, retrieve the object
+        //    //session objects are stored in the memory of theASP.NET server by default
+        //    Cart cart = (Cart)Session["Cart"];
+        //    if (cart == null)
+        //    {
+        //        cart = new Cart();
+        //        Session["Cart"] = cart;
+        //    }
+        //    return cart;
+        //}
     }
 }
