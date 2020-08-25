105a4d7d38d1be9b66ecffad46e0d8b2f46e373a
kasia.kordys <kasia.kordys@gmail.com>
2020-08-25 20:01:32 +0100

    add adding_product_to_cart_goes_to_cart_screen unit test for cart

--- SuperStore.UnitTests/CartTests.cs
+++ SuperStore.UnitTests/CartTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SuperStore.Domain.Abstract;
@@ -158,6 +159,34 @@ namespace SuperStore.UnitTests
 
             Trace.WriteLine(cart.Lines.ToArray()[1].Product.ProductID);
             //2
+        }
+
+        [TestMethod]
+        public void Adding_Product_To_Cart_Goes_To_Cart_Screen()
+        {
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+            }.AsQueryable());
+
+            // Arrange - create a Cart
+            Cart cart = new Cart();
+
+            // Arrange - create the controller
+            CartController target = new CartController(mock.Object);
+
+            // Act - add a product to the cart
+            RedirectToRouteResult result = target.AddToCart(cart, 2, "myUrl");
+
+            // Assert
+            //return RedirectToAction("Index", new { returnUrl });
+            //http:/localhost:/Cart/Index?returnUrl=myUrl
+            Assert.AreEqual(result.RouteValues["action"], "Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+
+            Trace.WriteLine(result);
+            //System.Web.Mvc.RedirectToRouteResult
         }
     }
 }
