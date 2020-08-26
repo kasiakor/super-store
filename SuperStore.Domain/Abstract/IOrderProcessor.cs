af2ddc7a5cfdbec30ca6cf4c57d23b69025d57a7
kasia.kordys <kasia.kordys@gmail.com>
2020-08-26 15:04:24 +0100

    add interface to process the order

---
+++ SuperStore.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,9 @@
+﻿using SuperStore.Domain.Entities;
+
+namespace SuperStore.Domain.Abstract
+{
+    public interface IOrderProcessor
+    {
+        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);
+    }
+}
