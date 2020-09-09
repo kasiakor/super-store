e60bcc3fce3798d82879ca8d71da952c284f6818
kasia.kordys <kasia.kordys@gmail.com>
2020-09-09 21:57:09 +0100

    add can_edit_product unit test

--- SuperStore.UnitTests/AdminTest.cs
+++ SuperStore.UnitTests/AdminTest.cs
@@ -43,5 +43,36 @@ namespace SuperStore.UnitTests
             Trace.WriteLine(myResults);
             //SuperStore.Domain.Entities.Product[]
         }
+
+
+        [TestMethod]
+        public void Can_Edit_Product()
+        {
+
+            // Arrange - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            });
+
+            // Arrange - create the controller
+            AdminController target = new AdminController(mock.Object);
+
+            // Act
+            //public ViewResult Edit(int productId)
+            Product p1 = target.Edit(1).ViewData.Model as Product;
+            Product p2 = target.Edit(2).ViewData.Model as Product;
+            Product p3 = target.Edit(3).ViewData.Model as Product;
+
+            // Assert
+            Assert.AreEqual(1, p1.ProductID);
+            Assert.AreEqual(2, p2.ProductID);
+            Assert.AreEqual(3, p3.ProductID);
+
+            Trace.WriteLine(p1.ProductID);
+            //1
+        }
     }
 }
