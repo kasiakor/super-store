b8fa389b8d925ec0d33e625eefa3906a7983ea09
kasia.kordys <kasia.kordys@gmail.com>
2020-09-15 18:57:17 +0100

    add save product method to update product details

--- SuperStore.Domain/Abstract/IProductRepository.cs
+++ SuperStore.Domain/Abstract/IProductRepository.cs
@@ -7,5 +7,8 @@ namespace SuperStore.Domain.Abstract
     {
         // A class that depends on the IProductRepository interface can obtain Product objects without needing to know anything about where they are coming from or how the implementation class will deliver them
         IEnumerable<Product> Products { get; }
+
+        //save changes to product from Edit method in Admin controller
+        void SaveProduct (Product product);
     }
 }
