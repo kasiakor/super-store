b3fe73204876c96668428c9f55d0e0eceb216342
kasia.kordys <kasia.kordys@gmail.com>
2020-09-16 18:29:51 +0100

    add data validation when editing product

--- SuperStore.Domain/Entities/Product.cs
+++ SuperStore.Domain/Entities/Product.cs
@@ -7,11 +7,18 @@ namespace SuperStore.Domain.Entities
     {
         [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage ="Please enter the product name")]
         public string Name { get; set; }
 
-        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Please enter the product description")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price ")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please enter the product category")]
         public string Category { get; set; }
     }
 }
