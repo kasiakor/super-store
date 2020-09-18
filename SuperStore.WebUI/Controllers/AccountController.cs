47c216a564b8e9dacbfe05f05d4ed46d399376ea
kasia.kordys <kasia.kordys@gmail.com>
2020-09-18 17:18:22 +0100

    add account controller

---
+++ SuperStore.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using SuperStore.WebUI.Infrastructure.Abstract;
+using SuperStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperStore.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IAuthProvider authProvider;
+
+        public AccountController(IAuthProvider auth)
+        {
+            authProvider = auth;
+        }
+        // GET: Login
+        public ViewResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (authProvider.Authenticate(model.UserName, model.Password))
+                {
+                    //url gets url helper object to generate urls
+                    //Action generates qualified url to action method
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("error", "Incorrect username or password");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
